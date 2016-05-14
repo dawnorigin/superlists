b98972076285120e2e4639eabb8435333991a42b
Xi Song <songxic@gmail.com>
2016-05-14 15:38:07 +0800

    new URL + view for adding to existing lists. FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,8 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
@@ -62,6 +62,4 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('Use peacock feathers to make a fly', page_text)
-        self.assertNotIn('Buy milk', page_text)
-
-        self.fail('Finish the test!')
+        self.assertIn('Buy milk', page_text)
